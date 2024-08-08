0a88797079be84905f5319fd4fc19ab36974239e
YechielCharnet <yechiel1995@gmail.com>
2024-08-08 19:13:32 +0300

    update first version of registration page

--- get-to-100/src/App.js
+++ get-to-100/src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Registration from "./Registration";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Registration />
+      </header>
     </div>
   );
 }
---
+++ get-to-100/src/Registration.jsx
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+
+const Registration = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [currentPlayers] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleLogin = () => {
+    const users = JSON.parse(localStorage.getItem("gameUsers")) || [];
+    const user = users.find(
+      (u) => u.username === username && u.password === password
+    );
+    if (user) {
+      currentPlayers.push(username);
+      //   setCurrentPlayers([...currentPlayers, username]);
+    } else {
+      setErrorMessage(
+        "Invalid username or password. Please try again or register."
+      );
+    }
+    console.log(users);
+    console.log(user);
+    console.log(currentPlayers);
+  };
+
+  const handleRegister = () => {
+    const users = JSON.parse(localStorage.getItem("gameUsers")) || [];
+    const existingUser = users.find(
+      (u) => u.email === email || u.username === username
+    );
+    if (existingUser)
+      setErrorMessage("Email or username already exist. Please try again.");
+    else {
+      const newUser = { email, username, password };
+      users.push(newUser);
+      localStorage.setItem("gameUsers", JSON.stringify(users));
+      currentPlayers.push(username);
+      // setCurrentPlayers([...currentPlayers, newUser]);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Welcome to Get to 100!</h1>
+      <div className="players">
+        <ul>
+          <h2>Current Players:</h2>
+          {currentPlayers.map((player, index) => (
+            <li key={index}>{player}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="form">
+        <p>Please sign in or register to join the game.</p>
+        {errorMessage && (
+          <div>
+            <div className="error">{errorMessage}</div>
+            <div className="form-group">
+              <label htmlFor="email">Email: </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+        )}
+
+        {/* {currentPlayers.length > 0 ? (
+        <div>
+          <h2>Current Players:</h2>
+          <ul>
+            {currentPlayers.map((player, index) => (
+              <li key={index}>{player.username}</li>
+            ))}
+          </ul>
+          <button className="btn">Start Game</button>
+        </div>
+      ) : ( */}
+        <>
+          <div className="form-group">
+            <label htmlFor="username">User name: </label>
+            <input
+              type="username"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="btn-group">
+            <button className="btn" onClick={handleLogin}>
+              Login
+            </button>
+            {errorMessage && (
+              <button className="btn" onClick={handleRegister}>
+                Register
+              </button>
+            )}
+          </div>
+        </>
+        {/* )} */}
+      </div>
+    </div>
+  );
+};
+
+export default Registration;
